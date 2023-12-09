c05997928fa721c24c6ee17359147b78ba50636b
Josef Jelinek <josef.jelinek@gmail.com>
2023-12-09 09:27:06 -0800

    Change solve function resolution to use a map.

--- main.go
+++ main.go
@@ -6,6 +6,27 @@ import (
 	"os"
 )
 
+var solveFuncByID = map[string]func(string){
+	"1-1": solveDay1Part1,
+	"1-2": solveDay1Part2,
+	"2-1": solveDay2Part1,
+	"2-2": solveDay2Part2,
+	"3-1": solveDay3Part1,
+	"3-2": solveDay3Part2,
+	"4-1": solveDay4Part1,
+	"4-2": solveDay4Part2,
+	"5-1": solveDay5Part1,
+	"5-2": solveDay5Part2,
+	"6-1": solveDay6Part1,
+	"6-2": solveDay6Part2,
+	"7-1": solveDay7Part1,
+	"7-2": solveDay7Part2,
+	"8-1": solveDay8Part1,
+	"8-2": solveDay8Part2,
+	"9-1": solveDay9Part1,
+	"9-2": solveDay9Part2,
+}
+
 func main() {
 	if len(os.Args) != 3 {
 		fmt.Printf("Expected problem ID (e.g \"1-1\", \"1-2\", \"2-1\", ...) and file name\n")
@@ -26,46 +47,10 @@ func solve(id, filename string) error {
 	if err != nil {
 		return err
 	}
-	input := string(b)
-	switch os.Args[1] {
-	case "1-1":
-		solveDay1Part1(input)
-	case "1-2":
-		solveDay1Part2(input)
-	case "2-1":
-		solveDay2Part1(input)
-	case "2-2":
-		solveDay2Part2(input)
-	case "3-1":
-		solveDay3Part1(input)
-	case "3-2":
-		solveDay3Part2(input)
-	case "4-1":
-		solveDay4Part1(input)
-	case "4-2":
-		solveDay4Part2(input)
-	case "5-1":
-		solveDay5Part1(input)
-	case "5-2":
-		solveDay5Part2(input)
-	case "6-1":
-		solveDay6Part1(input)
-	case "6-2":
-		solveDay6Part2(input)
-	case "7-1":
-		solveDay7Part1(input)
-	case "7-2":
-		solveDay7Part2(input)
-	case "8-1":
-		solveDay8Part1(input)
-	case "8-2":
-		solveDay8Part2(input)
-	case "9-1":
-		solveDay9Part1(input)
-	case "9-2":
-		solveDay9Part2(input)
-	default:
+	solve := solveFuncByID[id]
+	if solve == nil {
 		return fmt.Errorf("invalid ID: %q", id)
 	}
+	solve(string(b))
 	return nil
 }
